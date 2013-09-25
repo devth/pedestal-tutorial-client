17d817d4cff3b36657acd847638943829e76d6c2
Trevor Hartman <trevorhartman@gmail.com>
2013-09-25 09:11:17 -0600

    Output debug messagings and apply some post-processing

--- app/src/tutorial_client/start.cljs
+++ app/src/tutorial_client/start.cljs
@@ -4,11 +4,12 @@
             [io.pedestal.app.render.push :as push-render]
             [io.pedestal.app.render :as render]
             [io.pedestal.app.messages :as msg]
+            [tutorial-client.post-processing :as post]
             [tutorial-client.behavior :as behavior]
             [tutorial-client.rendering :as rendering]))
 
 (defn create-app [render-config]
-  (let [app (app/build behavior/example-app)
+  (let [app (app/build (post/add-post-processors behavior/example-app))
         render-fn (push-render/renderer "content" render-config render/log-fn)
         app-model (render/consume-app-model app render-fn)]
     (app/begin app)
