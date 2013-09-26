5dc56f06876253e1ab537fd081d4cb9b2f251b72
Trevor Hartman <trevorhartman@gmail.com>
2013-09-26 10:37:15 -0600

    Slice some templtes

--- app/src/tutorial_client/html_templates.clj
+++ app/src/tutorial_client/html_templates.clj
@@ -3,4 +3,12 @@
 
 (defmacro tutorial-client-templates
   []
-  {:tutorial-client-page (dtfn (tnodes "tutorial-client.html" "hello") #{:id})})
+  {:tutorial-client-page (dtfn (tnodes "tutorial-client.html" "tutorial" [[:#other-counters]])
+                               #{:id})
+   :other-counter (dtfn (tnodes "tutorial-client.html" "other-counter") #{:id})})
+
+; (def t (tutorial-client-templates))
+; (def tf (:other-counter t))
+; (tf)
+; (let [[_ ctor] (tf)]
+;   (ctor {:id 42}))
